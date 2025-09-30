c9884b5aa6d958da0066b9990d4873f217ef503d
Girthquake <captainpatchwork@gmail.com>
2025-09-29 21:58:04 -0800

    Update types.py

--- filetools/types.py
+++ filetools/types.py
@@ -22,13 +22,19 @@ class File(FileSystemObject):
 	def __init__(self, path, lazy=True):
 		super().__init__(path, lazy)
 		
-	def data(self, start: int = 0, stop: int = -1, chunk_size: int = 1024) -> Iterator[bytes]:
-		cur = 0
+	def data(self, start: int = 0, stop: int = -1, chunk_size: int = 1024, max_chunks: int = -1) -> Iterator[bytes]:
+		cur_idx = 0
+		total_ch = 0
 		with open(self.path, "rb") as file:
 			while chunk := file.read(chunk_size):
-				if (cur < stop or stop < 0):
-					cur += (len(chunk) - 1)
+				if (cur_idx < stop or stop == -1) and (total_ch < chunks or chunks == -1):
+					cur_idx += (len(chunk) - 1)
+					total_ch += 1
 					yield chunk
+
+					if stop != -1 and (remaining := stop - cur_idx) < chunk_size:
+						chunk_size = remaining
+
 				else:
 					break
 
