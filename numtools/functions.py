41d1f486f195b0937b462367a679765f2f2f7567
Girthquake <captainpatchwork@gmail.com>
2025-09-30 20:44:11 -0800

    Update functions.py

--- numtools/functions.py
+++ numtools/functions.py
@@ -1,9 +1,9 @@
 
 def fibonacci_value(i: int) -> int:
 	if not isinstance(i, int):
-		raise TypeError(f"'fibonacci_value' accepts integers as values, provided {type(i).__name__}")
+		raise TypeError(f"'fibonacci_value' accepts integers as values, got {type(i).__name__}")
 	if v < 0:
-		raise ValueError(f"'fibonacci_value' accepts positive integers as values, provided {v}")
+		raise ValueError(f"'fibonacci_value' accepts positive integers as values, got {v}")
 
 	a, b = 0, 1
 	for _ in range(int(i)):
@@ -13,9 +13,9 @@ def fibonacci_value(i: int) -> int:
 
 def fibonacci_index(v: int) -> int:
 	if not isinstance(v, int):
-		raise TypeError(f"'fibonacci_index' accepts integers as values, provided {type(v).__name__}")
+		raise TypeError(f"'fibonacci_index' accepts integers as values, got {type(v).__name__}")
 	if v < 0:
-		raise ValueError(f"'fibonacci_index' accepts positive integers as values, provided {v}")
+		raise ValueError(f"'fibonacci_index' accepts positive integers as values, got {v}")
 
 	a, b = 0, 1
 	idx = 0
@@ -25,19 +25,19 @@ def fibonacci_index(v: int) -> int:
 	return idx if a == value else -1
 
 
-def fibonacci_nearest(v: int) -> int:
-	if not isinstance(v, int):
-		raise TypeError(f"'fibonacci_nearest' accepts integers as values, provided {type(v).__name__}")
-	if v < 0:
+def fibonacci_nearest(n: int) -> int:
+	if not isinstance(n, int):
+		raise TypeError(f"'fibonacci_nearest' accepts integers as values, got {type(n).__name__}")
+	if n < 0:
 		return 0
 
 	a, b = 0, 1
 	while True:
 		c, b = b, a + b
-		if c == v:
+		if c == n:
 			return c
-		elif c < v:
+		elif c < n:
 			a = c
 			continue
-		elif c > v:
-			return a if (v - a) < (c - v) else c
+		elif c > n:
+			return a if (n - a) < (n - v) else c
