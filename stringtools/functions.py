2b3b423ee521bf2e04cd13cc16ef657a1bb22137
Girthquake <captainpatchwork@gmail.com>
2025-09-30 20:41:35 -0800

    Update functions.py

--- stringtools/functions.py
+++ stringtools/functions.py
@@ -1,6 +1,8 @@
-from typing import Sequence, Union
+from typing import Container, Iterator
 import collections
 import string
+
+DEFAULT_TOKEN_CHARSET = tuple("'", *string.ascii_letters)
 
 def alpha_index(char: str) -> int:
 	if not isinstance(char, str):
@@ -12,3 +14,21 @@ def alpha_index(char: str) -> int:
 		return ord(char) - ord('A' if char.isupper() else 'a')
 	else:
 		return -1
+
+def tokenize(data: str, remove_whitespace: bool = False, remove_punctuation: bool = False, continuity_charset: Container[str] = DEFAULT_TOKEN_CHARSET) -> Iterator[str]:
+	cur_token = ''
+	for char in str(data):
+		if char in continuity_charset:
+			cur_token += char
+		else:
+			if cur_token:
+				yield cur_token
+				cur_token = ''
+			if remove_whitespace and (char in string.whitespace):
+				continue
+			if remove_punctuation and (char in string.punctuation):
+				continue
+
+			yield char
+	if cur_token:
+		yield cur_token
